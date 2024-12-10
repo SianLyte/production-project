c289bb3b45e5d22756a7b9ea15475f86c9fb060f
Yaroslav Teplov <yaroslav_teplov@mail.ru>
2024-12-10 21:16:50 +0400

    fix dev plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,7 @@ import { BuildOptions } from "./types/config"
 import { buildCssLoader } from "./loaders/buildCssLoader"
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    
 
     const svgLoader = {
         test: /\.svg$/,
